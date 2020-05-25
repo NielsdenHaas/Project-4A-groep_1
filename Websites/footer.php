9ce633dcdc16979f0787d38d858f7ab261a13d39
NielsdenHaas <61315255+NielsdenHaas@users.noreply.github.com>
2020-05-25 09:34:50 +0200

    Home pagina realisatie

---
+++ Websites/footer.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="language" content="nl">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="N. den Haas">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<script type="text/javascript" src="script.js"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">       
+</head>
+
+<style>
+
+
+@charset "utf-8";
+@media only screen and (max-width: 768.9px){
+    * {
+		margin: 0;
+		padding: 0;
+    }
+    
+    #mascotte {
+        width: 8vw;
+        margin: 0 0 2vw 2vw;
+    }
+
+    #rekenmachine {
+        width: 6vw;
+        margin: 0 2vw 2vw 0;
+    }
+
+    footer {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    footer p {
+        color: #707070;
+        font-size: 0.8rem;
+        margin-top: 2vw
+    }
+}
+
+@media only screen and (min-width: 769px){
+	* {
+		margin: 0;
+		padding: 0;
+    }
+    
+    #mascotte {
+        width: 4vw;
+    }
+
+    #rekenmachine {
+        width: 3vw
+    }
+
+    footer {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    footer p {
+        color: #707070;
+    }
+}
+
+</style>
+
+<body>
+    
+    <footer>
+
+        <img id="mascotte" src="images/mascotte.jpg" alt="mascotte van de website">
+        <p>@ 2020 Math Mate || All rights reserved</p>
+        <img id="rekenmachine" src="images/RekenMachine.PNG" alt="Icoon van een rekenmachine">
+    
+    </footer>
+    
+ </body>
+    
+</html>
---
+++ Websites/home.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="language" content="nl">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="N. den Haas">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<script type="text/javascript" src="script.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/home.css">       
+</head>
+
+<body> 
+
+    <?php   
+    include "header.php";
+    ?>
+    
+    <main>  
+        <section id="inhoud">
+            <h1>Beste Site Voor Al Uw Berekeningen</h1>
+            <p>Lorizzle ipsizzle dolor sit amizzle,
+                check it out adipiscing elizzle. Nul
+                sapien velizzle, volutpizzle, suscipit 
+                gravida vizzle, stuff. Pellentesque giz
+                tortizzle. Own yo' eros. Fusce izzle 
+                dapibizzle turpis tempizzle shut </p>
+                <input id="Button" type="button" value="Lees Meer!">
+        </section>
+        <section>
+            <img id="stickman_image" src="images/stickman_home.png" alt="Home afbeelding stickman met rekenmachine">
+        </section>
+    </main>
+
+    <?php   
+    include "footer.php";
+    ?>
+    
+ </body>
+    
+</html>
