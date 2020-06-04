a3aaf192eeb61110d1f1d15c9c08e01749913ae7
MarlydeHaard <61316126+MarlydeHaard@users.noreply.github.com>
2020-06-04 13:34:18 +0200

    begin reeksen pagina

---
+++ Websites/reeksen.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="language" content="nl">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="M. de Haard">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MathMate | Presentatie</title>
+    <script type="text/javascript" src="script/presentatie.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/reeksen.css">
+</head>
+
+<body>
+
+<main>
+    <section class="container_reeksen">
+        <article class="article_reeksen">
+            <h1>Reeksen</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing enim eu turpis egestas pretium aenean pharetra. Blandit cursus risus at ultrices mi tempus imperdiet nulla. Morbi leo urna molestie at elementum eu facilisis sed odio. Nunc lobortis mattis aliquam faucibus purus. Cursus euismod quis viverra nibh cras pulvinar mattis. Feugiat vivamus at augue eget arcu dictum varius duis at. Elit ut aliquam purus sit amet luctus venenatis lectus magna. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Tincidunt arcu non sodales neque sodales ut etiam. Tempus egestas sed sed risus pretium. Volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris.
+            </p>
+        </article>
+
+        <img src="images/reeksen.png" class="img_reeksen">
+    </section>
+
+    <form class="form_reeksen">
+        <label>Reeksen tot:</label>
+        <input type="number" placeholder="Invoer..." id="invoer">
+        <input type="button" value="Bereken">
+        <select id="reeks">
+            <option>Fibonacci</option>
+            <option>Priem Getallen</option>
+        </select>
+        <div id="antwoord">
+
+        </div>
+    </form>
+
+    <section class="container_berekening">
+        <article class="article_berekening">
+            <h1>Berekening</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing enim eu turpis egestas pretium aenean pharetra. Blandit cursus risus at ultrices mi tempus imperdiet nulla. Morbi leo urna molestie at elementum eu facilisis sed odio. Nunc lobortis mattis aliquam faucibus purus. Cursus euismod quis viverra nibh cras pulvinar mattis. Feugiat vivamus at augue eget arcu dictum varius duis at. Elit ut aliquam purus sit amet luctus venenatis lectus magna. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Tincidunt arcu non sodales neque sodales ut etiam. Tempus egestas sed sed risus pretium. Volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris.
+
+                Felis eget velit aliquet sagittis id consectetur purus. Id consectetur purus ut faucibus. Pellentesque sit amet porttitor eget dolor morbi non arcu risus. Sodales neque sodales ut etiam sit amet nisl. Sit amet luctus venenatis lectus magna fringilla urna porttitor. Ultrices neque ornare aenean euismod elementum nisi quis eleifend. Odio pellentesque diam volutpat commodo. Vitae aliquet nec ullamcorper sit amet risus. Sem nulla pharetra diam sit amet nisl suscipit adipiscing. Enim nunc faucibus a pellentesque sit amet porttitor eget.
+            </p>
+        </article>
+        
+        <img src="" class="img_berekening">
+    </section>
+</main>
+
+</body>
+</html>
