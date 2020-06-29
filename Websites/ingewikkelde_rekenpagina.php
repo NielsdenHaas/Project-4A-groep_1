5681ee717e6984bc82aaf80b49bb45b01ffc3f95
MarlydeHaard <61316126+MarlydeHaard@users.noreply.github.com>
2020-06-29 11:30:46 +0200

    ingewikkelde pagina responsieve

--- Websites/ingewikkelde_rekenpagina.php
+++ Websites/ingewikkelde_rekenpagina.php
@@ -21,21 +21,36 @@ include "header-ingelogd.php";
 ?>
 
 <main>
+    <section class="container_uitleg">
+        <article class="article_uitleg">
+            <h1>Talstelsels</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing enim eu turpis egestas pretium aenean pharetra. Blandit cursus risus at ultrices mi tempus imperdiet nulla. Morbi leo urna molestie at elementum eu facilisis sed odio. Nunc lobortis mattis aliquam faucibus purus. Cursus euismod quis viverra nibh cras pulvinar mattis. Feugiat vivamus at augue eget arcu dictum varius duis at. Elit ut aliquam purus sit amet luctus venenatis lectus magna. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Tincidunt arcu non sodales neque sodales ut etiam. Tempus egestas sed sed risus pretium. Volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris.
+            </p>
+
+            <h1>Factoriser</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing enim eu turpis egestas pretium aenean pharetra. Blandit cursus risus at ultrices mi tempus imperdiet nulla. Morbi leo urna molestie at elementum eu facilisis sed odio. Nunc lobortis mattis aliquam faucibus purus. Cursus euismod quis viverra nibh cras pulvinar mattis. Feugiat vivamus at augue eget arcu dictum varius duis at. Elit ut aliquam purus sit amet luctus venenatis lectus magna. Elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Tincidunt arcu non sodales neque sodales ut etiam. Tempus egestas sed sed risus pretium. Volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris.
+            </p>
+        </article>
+    </section>
 
     <form>
         <select onchange="opzetKeuze()" id="opKeuze">
             <option value="Talstelsel">Berekenen Talstelsel!</option>
             <option value="Factorizer">Factorizer!</option>
         </select>
-        <input class="input" type="number" id="omreken-talstelsel" placeholder="getal invoeren....">
-        <input class="input" type="number" id="van" placeholder="van....">
-        <input class="input" type="number" id="naar" placeholder="naar....">
-        <input class="button" type="button" value="Bereken!" onclick="Bereken()">
-        <input class="button" type="button" value="Wissen" onclick="resetform()">
-        <div class="row form-group">
-            <div class="col-10">
-                <div id="dResultaat" class="jumbotron"></div>
-            </div>
+        <div class="inputs">
+            <input class="input" type="number" id="omreken-talstelsel" placeholder="getal invoeren....">
+            <input class="input" type="number" id="van" placeholder="van....">
+            <input class="input" type="number" id="naar" placeholder="naar....">
+        </div>
+        <div class="buttons">
+            <input class="button" type="button" value="Bereken!" onclick="Bereken()">
+            <input class="button" type="button" value="Wissen" onclick="resetform()">
+        </div>
+        <div class="form-group">
+            <div id="dResultaat" class="jumbotron"></div>
         </div>
     </form>
 
